69fdb69d28e76bab99213fde733f8bf6d151ba17
ArzaM09 <115831548+ArzaM09@users.noreply.github.com>
2024-12-07 09:45:26 +0700

    belajar floating-text

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-from kivy.app import app
+import kivy
+from kivy.app import App
 from kivy.uix.label import Label
+kivy.require('1.9.0')
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
+
+class tutorialApp(App):
+    def build(self):
+        b = BoxLayout (orientation='vertical')
+        t = TextInput (font_size=50,
+                       size_hint_y=None,
+                       height=100)
+        f = FloatLayout()
+        s = Scatter()
+        l = Label (text='Hello, Kivy!', font_size=50)
+
+        f.add_widget(s)
+        s.add_widget(l)
+        b.add_widget(t)
+        b.add_widget(f)
+        t.bind(text = l.setter('text'))
+        return b
+
+if __name__ == '__main__':
+    tutorialApp().run()
